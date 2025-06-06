4d90d45716b3fbbf9023e32fcf9a52d23e849c3f
interieuridee <luchydracrypto@gmail.com>
2025-06-06 15:13:14 +0200

    Update ProjectDetail.tsx

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -336,8 +336,8 @@ export default function ProjectDetail() {
           <div className="container-custom">
             <h2 className="text-3xl font-serif mb-8 text-left">Plattegronden</h2>
             <div className="flex flex-col md:flex-row gap-8 items-center justify-center">
-              <img src="/plattegronden/vakantiehuisplattegrond (2).png" alt="Plattegrond Zondagsschool 1" className="w-full max-w-md h-auto rounded-lg" />
-              <img src="/plattegronden/vakantiehuisplattegrond5 (1).png" alt="Plattegrond Zondagsschool 2" className="w-full max-w-md h-auto rounded-lg" />
+              <img src="/plattegronden/vakantiehuisplattegrond (2).webp" alt="Plattegrond Zondagsschool 1" className="w-full max-w-md h-auto rounded-lg" />
+              <img src="/plattegronden/vakantiehuisplattegrond5 (1).webp" alt="Plattegrond Zondagsschool 2" className="w-full max-w-md h-auto rounded-lg" />
             </div>
           </div>
         </section>
